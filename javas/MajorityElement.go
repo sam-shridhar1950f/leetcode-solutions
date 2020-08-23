23bc4f5b3f9d115a1172ae141215d1670529613b
Sam Shridhar <62958051+sam-shridhar1950f@users.noreply.github.com>
2020-08-23 14:19:41 -0400

    Create MajorityElement.go

---
+++ javas/MajorityElement.go
@@ -0,0 +1,25 @@
+func majorityElement(nums []int) int {
+    length := len(nums)
+	major := length / 2
+	counter1 := make(map[int]int)
+
+	for i, _ := range nums {
+		if counter1[nums[i]] > 0 {
+			// Add 1 to the counter
+			counter1[nums[i]] += 1
+		} else {
+			// Otherwise, create a new counter for that new letter
+			counter1[nums[i]] = 1
+		}
+	}
+
+	for key, element := range counter1 {
+		if element > major {
+			return key
+		} else {
+			continue
+		}
+	}
+	return 0
+    
+}

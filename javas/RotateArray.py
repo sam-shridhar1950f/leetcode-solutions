6a9366960ba41a3ae83cef684ed55ad65bd8b020
Sam Shridhar <62958051+sam-shridhar1950f@users.noreply.github.com>
2020-08-23 21:46:03 -0400

    Update RotateArray.py

--- javas/RotateArray.py
+++ javas/RotateArray.py
@@ -11,17 +11,16 @@ def rotateArray(nums, k):
 
     lis = []
     for i in range(0, k):
-        lis.append(nums[len(nums) -1 - i]) # For loop adding elements to be shifted to a list
+        lis.append(nums[len(nums) -1 - i])
 
     good_lis = lis[::-1] + nums[0:len(nums) - k]
     nums.clear()
     nums.extend(good_lis)
-    # nums = lis[::-1] + nums[0:len(nums) - k]
+
     
 
 
-    return print(nums) # list of elements needed to be shifted reversed, so that elements not to be shifted can be added back
-    # Can do this with one for loop by combining two lists, one with shifted values and one with inplace values
+    return nums
 
 
 rotateArray([1, 2], 3)

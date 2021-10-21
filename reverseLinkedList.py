872176a15a97c76fb0c587a72a9e0ee1a3405ec1
Sam Shridhar <sam.shridhar1950f@gmail.com>
2021-10-21 11:17:11 -0400

    Update reverseLinkedList.py

--- reverseLinkedList.py
+++ reverseLinkedList.py
@@ -2,9 +2,9 @@ def reverseList(self, head):
     prev_node = None
     curr_node = head
     while curr_node:
-        next_node = curr_node.next # Remember next node [1, 2, 3]
-        curr_node.next = prev_node  # REVERSE! None, first time round.
-        prev_node = curr_node  # Used in the next iteration.
-        curr_node = next_node  # Move to next node.
+        next_node = curr_node.next 
+        curr_node.next = prev_node 
+        prev_node = curr_node  
+        curr_node = next_node  
     head = prev_node
     return head
